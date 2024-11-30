50239853b7b24f84bd45dc9ae648c12901daddb9
Leo Gaskin <leo.gaskin@le0.gs>
2024-11-30 23:12:34 +0100

    Commit initial plugin template

---
+++ agent.py
@@ -0,0 +1,22 @@
+from typing import Any
+from flask import Request
+from searx.search import SearchWithPlugins as Search
+
+name = "Agent"
+description = "Add an LLM agent to your SearXNG search"
+default_on = False
+
+
+def pre_search(request: Request, search: Search) -> bool:
+    """Runs BEFORE the search request."""
+    return True
+
+
+def post_search(request: Request, search: Search) -> None:
+    """Runs AFTER the search request."""
+    pass
+
+
+def on_result(request: Request, search: Search, result: Any) -> bool:
+    """Runs for each result of each engine."""
+    return True
